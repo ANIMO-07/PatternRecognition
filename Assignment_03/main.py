1fcc0a0516ba7e1fac237aca2787ca4b62de60dd
Gajraj Singh Chouhan <gajrajgchouhan@gmail.com>
2021-11-23 22:02:11 +0530

    done assignment 3 :joy:

--- Assignment_03/main.py
+++ Assignment_03/main.py
@@ -1,171 +1,178 @@
 from os.path import join
-import pandas as pd 
-import matplotlib.pyplot as plt 
-import numpy as np 
+from numpy.random import triangular
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 import seaborn as sns
-from math import exp, pow, sqrt, log
-from sklearn.cluster import KMeans
+from math import pow, sqrt
 import math
+import matplotlib
+from sklearn.model_selection import train_test_split
+from scipy.stats import multivariate_normal
+from tqdm.auto import tqdm
 
 def make_df(path, *classes, sep=","):
     dfs = []
     for i, c in enumerate(classes):
-        df = pd.read_csv(join(path, c), names=["x", "y"], sep=sep, dtype={"x" : "float", "y" : "float"}, engine="python")
-        df["class"] = i 
+        df = pd.read_csv(join(path, c), names=["x", "y"], sep=sep, dtype={"x": "float", "y": "float"}, engine="python")
+        df["class"] = i
         dfs.append(df)
-    return pd.concat(dfs, axis=0).reset_index(drop=True) 
+    return pd.concat(dfs, axis=0).reset_index(drop=True)
 
-def gaussian(x, mean, cov):
-    d = cov.shape[0]
-    exponent = np.exp(-0.5 * (x - mean).T @ np.linalg.inv(cov) @ (x - mean))
+class GMM:
 
-    t = pow(pow(2 * math.pi, d / 2) * sqrt(abs(np.linalg.det(cov))), -1)
-    return t * exponent
-
-def loglikeli(data, class_, ncomponents, nfeatures, mu, cov, pi):
-    ll = 0
-    tmp_mu = mu[class_, ...]
-    tmp_cov = cov[class_, ...]
-    tmp_pi = pi[class_, ...]
-
-    for n in range(data.shape[0]):
+    def gaussian(self, x, mean, cov):
         
-        s = 0
-        for q in range(ncomponents):
-            s += tmp_pi[q] * gaussian(data[n,...], tmp_mu[q,...], tmp_cov[q,...])
-        try:
-            ll += log(s)
-        except ValueError:
-            pass
-    return ll
+        return multivariate_normal.pdf(x, mean=mean, cov=cov)
 
-def assign_classes(data, nclasses, ncomponents, mu, cov, pi):
-    assigned = np.ones((data.shape[0], 1))
-    for n in range(data.shape[0]):
-        probs = []
-        for class_ in range(nclasses):
-            p = 0
+    def loglikeli(self, data, ncomponents, mu, cov, pi):
+        ll = 0
+        for n in range(data.shape[0]):
+            s = 0
             for q in range(ncomponents):
-                p += pi[class_, q] * gaussian(data[n, ...], mu[class_, q, ...], cov[class_, q, ...])
-            probs.append(p)
-        assigned[n] = np.argmax(probs)
-    return assigned
+                s += pi[q] * self.gaussian(data[n], mu[q], cov[q])
+            ll += np.log(s)
+        return ll
 
-def gmm_fit(data, ncomponents=2):
-    data = np.asarray(data)
-    nclasses = len(np.unique(data[:,2]))
-    nfeatures = np.shape(data)[1] - 1
+    def weight_likelihood(self, x, q, mu, cov, pi):
+        
+        return pi[q] * self.gaussian(x, mu[q], cov[q])
 
-    threshold = 1
+    def initial_params(self, X, ncomponents, mu, cov):
+        splitted = np.split(X, ncomponents)
+        for q in range(ncomponents):
+            mu[q] = np.mean(splitted[q], axis=0)
+            cov[q] = np.cov(splitted[q], rowvar=False, bias=True)
+        return (mu, cov)
 
-    mu = np.ones((nclasses, ncomponents, nfeatures))
-    cov = np.ones((nclasses, ncomponents, nfeatures, nfeatures))
-    pi = np.ones((nclasses, ncomponents)) / ncomponents
+    def e_step(self, train, ncomponents, mu, cov, pi):
+        assigned_clusters = np.zeros((train.shape[0], 1))
+        gamma = np.zeros((train.shape[0], ncomponents))
 
-    assigned_clusters = np.ones((data.shape[0]))
+        for n in range(train.shape[0]):
+            posterior = []
+            for q in range(ncomponents):
+                posterior.append(self.weight_likelihood(train[n], q, mu, cov, pi))
 
-    weight_likelihood = lambda x, class_, q : pi[class_, q] * gaussian(x, mu[class_, q, :], cov[class_, q, ...])
+            posterior = np.array(posterior) / np.sum(posterior)
+            gamma[n, :] = posterior.ravel()
+            assigned_clusters[n] = np.argmax(posterior)
+            assert np.sum(posterior) > 0.99, sum(posterior)
 
-    for class_ in range(nclasses):
-        train = data[data[:,2] == class_][:, :-1]
-        assigned_clusters_ = assigned_clusters[data[:,2] == class_]
+        return assigned_clusters, gamma
 
-        kmeans = KMeans(n_clusters=ncomponents, init="k-means++", max_iter=500, algorithm = 'auto')
-        pred = kmeans.fit_predict(train)
 
-        mu[class_, ...] = kmeans.cluster_centers_
+    def m_step(self, train, ncomponents, mu, cov, pi, assigned_clusters, gamma, nfeatures):
+        Nq = np.sum(gamma, axis=0)
+        N = train.shape[0]
+        pi = Nq / N
 
         for q in range(ncomponents):
-            cov[class_, q, ...] = np.cov(train[pred == q].T)
+            new_mu = gamma[:, q].reshape(-1, 1).T @ train
+            new_mu = new_mu.reshape(-1, nfeatures)
+            new_mu /= Nq[q]
+
+            new_cov = np.zeros((nfeatures, nfeatures))
+
+            for n in range(train.shape[0]):
+                diff = train[n] - mu[q, :]
+                diff = diff.reshape(-1, 1)
+                new_cov += gamma[n, q] * (diff @ (diff).T)
+
+            new_cov /= Nq[q]
+            # new_cov.flat[:: (nfeatures + 1)] += 1e-6
+
+            mu[q] = new_mu
+            cov[q, ...] = new_cov
+
+        return mu, cov, pi
+
+
+    def gmm_fit(self, X, ncomponents=2):
+        self.ncomponents = ncomponents
+        train = np.asarray(X)
+        nfeatures = X.shape[1]
+        threshold = 1
+
+        mu = np.zeros((ncomponents, nfeatures))
+        cov = np.zeros((ncomponents, nfeatures, nfeatures))
+        pi = np.ones((ncomponents, 1)) / ncomponents
+        mu, cov = self.initial_params(train, ncomponents, mu, cov)
 
         old_ll = 0
         iterations = 0
-        try:
-            while 1:
-                # print(f"{iterations=}")
-                ll = loglikeli(train, class_, ncomponents, nfeatures, mu, cov, pi)
-                # print(class_, old_ll, ll)
-                if ll == 0:
-                    break
 
-                if (abs(ll - old_ll) < threshold):
-                    break
-                else:
-                    old_ll = ll
-                
-                assigned_clusters_ = np.ones((train.shape[0], 1))
+        while 1:
+            ll = self.loglikeli(train, ncomponents, mu, cov, pi)
 
-                for n in range(train.shape[0]):
-                    denominator = np.sum([weight_likelihood(train[n, :], class_, q) for q in range(ncomponents)])
-                    posterior = []
-                    for q in range(ncomponents):
-                        posterior.append(weight_likelihood(train[n, :], class_, q) / (denominator + 1e-8))
-                    assigned_clusters_[n] = np.argmax(posterior)
+            if abs(ll - old_ll) < threshold:
+                break
+            old_ll = ll
 
-                N = train.shape[0]
-                Nq = np.array([np.sum(assigned_clusters_ == q) for q in range(ncomponents)])
-                pi[class_, :] = Nq / N
+            assigned_clusters, gamma = self.e_step(train, ncomponents, mu, cov, pi)
+            mu, cov, pi = self.m_step(train, ncomponents, mu, cov, pi, assigned_clusters, gamma, nfeatures)
+            iterations += 1
 
-                for q in range(ncomponents):
-                    new_mu = np.zeros((1, nfeatures))
-                    for n in range(train.shape[0]):
-                        new_mu += weight_likelihood(train[n, :], class_, q) * train[n, :]
-                    
-                    new_mu /= Nq[q, ...]
+        self.mu = mu 
+        self.cov = cov 
+        self.pi = pi
 
-                    new_cov = np.zeros((1, nfeatures, nfeatures))
-                    for n in range(train.shape[0]):
-                        new_cov += weight_likelihood(train[n, :], class_, q) * (train[n, :] - mu[class_, q, :]) @ (train[n, :] - mu[class_, q, :]).T
-                    new_cov /= Nq[q, ...]
-                    new_cov.flat[::(nfeatures + 1) ] += 1e-5
-                    
-                    mu[class_, q, :] = new_mu
-                    cov[class_, q, ...] = new_cov 
+        return self
 
-                assigned_clusters[data[:,2] == class_] = assigned_clusters_.ravel()
-                iterations += 1
-        except Exception as e:
-            pass
-            
-    return mu, cov, pi
+    def score_samples(self, X):
+        test = np.asarray(X)
+        nfeatures = test.shape[1]
+        scores = np.zeros((X.shape[0], 1))
 
-def gmm_predict(data, mu, cov, pi, ncomponents=2):
+        for n in range(X.shape[0]):
+            p = 0
+            for q in range(self.ncomponents):
+                p += self.weight_likelihood(test[n], q, self.mu, self.cov, self.pi)
+            scores[n] = p
+        return scores
+
+
+def gmm_predict(data, ncomponents=2):
+
     data = np.asarray(data)
-    nclasses = len(np.unique(data[:,2]))
-    nfeatures = np.shape(data)[1] - 1
-    assigned = []
-    for n in range(data.shape[0]):
-        for c in range(nclasses):
-            prob = []
-            s = 0
-            for q in range(ncomponents):
-                s += pi[c, q] * gaussian(data[n, :-1], mu[c, q, ...], cov[c, q, ...])
-            prob.append(s)
-        assigned.append(np.argmax(prob))
+    X = data[:, :2].astype(float)
+    y = data[:, 2]
+    nclasses = len(np.unique(y))
+    gmms = []
+    for n in tqdm(range(nclasses)):
+        gm = GMM().gmm_fit(X[y == n], ncomponents)
+        gmms.append(gm)
 
-    return assigned
+    scores = []
+    for n in range(nclasses):
+        scores.append(gmms[n].score_samples(X).reshape(-1, 1))
+
+    return np.argmax(np.concatenate(scores, axis=1), axis=1)
 
 
-if __name__ == '__main__':
+def plot_predict(data):
+    # uncomment for plots
+    plt.scatter(data["x"], data["y"], c=data["class"])
+    plt.title("plot of data")
+    plt.show()
+
+    sns.jointplot(data=data, x="x", y="y", hue="class")
+    plt.show()
+
+    assigned = gmm_predict(data)
+
+    data["assigned"] = assigned
+    accuracy = np.sum(assigned == np.asarray(data["class"])) / assigned.shape[0]
+
+    plt.scatter(data["x"], data["y"], c=data["assigned"])
+    plt.title(f"plot of predicted data | Accuracy = {accuracy*100:.03f}%")
+    plt.show()
+
+# matplotlib.use("Agg")
+if __name__ == "__main__":
     LS = "./data_1/"
     NLS = "./nls_data/"
     ls_data = make_df(LS, "Class1.txt", "Class2.txt")
     nls_data = make_df(NLS, "class1.txt", "class2.txt")
 
-    # uncomment for plots
-    plt.scatter(nls_data["x"], nls_data["y"], c=nls_data["class"])
-    plt.title("plot of data")
-    plt.show()
-
-    # sns.jointplot(data=nls_data, x="x", y="y", hue="class")
-    # plt.show()
-
-    mu, cov, pi = gmm_fit(nls_data)
-    assigned = gmm_predict(nls_data, mu, cov, pi)
-
-    nls_data["assigned"] = assigned
-
-    plt.scatter(nls_data["x"], nls_data["y"], c=nls_data["assigned"])
-    plt.title("plot of data")
-    plt.show()
-
+    plot_predict(nls_data)
